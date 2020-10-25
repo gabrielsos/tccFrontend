c2c83cadd0f389d28bc3c3bfa5723e404c2b4ca9
Gabriel Silva <gabrielsos99@gmail.com>
2020-10-25 04:56:09 -0300

    feat: added o.s history page

---
+++ src/pages/Historico/index.tsx
@@ -0,0 +1,113 @@
+import React, { useState, useEffect, useCallback } from 'react';
+import { FiArrowLeft, FiEdit, FiTrash } from 'react-icons/fi';
+import { Link, useHistory } from 'react-router-dom';
+
+import api from '../../services/api';
+
+import HeaderMenu from '../../components/HeaderMenu';
+import Header from '../../components/Header';
+
+import {
+  Container,
+  Content,
+  ContentHeader,
+  AsideContainer,
+  ButtonsContainer,
+  NameContainer,
+} from './styles';
+import Button from '../../components/Button';
+
+interface osData {
+  osId: number;
+  initDate: string;
+  localName: string;
+  osDescription: string;
+  name: string;
+  equip: string;
+  osStateName: string;
+  rightFinalDate: string | null;
+  rightInitDate: string;
+  typeName: string;
+}
+
+const Locals: React.FC = () => {
+  const history = useHistory();
+
+  const [allOs, setallOs] = useState<osData[]>([]);
+
+  useEffect(() => {
+    async function loadData() {
+      const response = await api.get('all-os');
+
+      setallOs(response.data);
+    }
+
+    loadData();
+  }, []);
+
+  const handleNewOs = useCallback(() => {
+    history.push('/admin/os/new');
+  }, [history]);
+
+  const handleShowRegisters = useCallback(
+    (id: string, osDateInit: string, osStateName: string) => {
+      localStorage.setItem('osIdRegister', id);
+      localStorage.setItem('osStateName', osStateName);
+      localStorage.setItem('osDateInit', osDateInit);
+    },
+    [],
+  );
+
+  return (
+    <Container>
+      <Header />
+      <HeaderMenu />
+      <Content>
+        <ContentHeader>
+          <AsideContainer>
+            {/* eslint-disable-next-line react/jsx-one-expression-per-line */}
+            <h1>Total de ordens de serviço: {allOs.length}</h1>
+          </AsideContainer>
+          <Button type="button" onClick={handleNewOs}>
+            Nova ordem de serviço
+          </Button>
+        </ContentHeader>
+        <ul>
+          {allOs.map(o => (
+            <Link
+              to={`/admin/os/${o.osId}`}
+              onClick={() =>
+                handleShowRegisters(String(o.osId), o.initDate, o.osStateName)
+              }
+            >
+              <li key={o.osId}>
+                <strong>Status</strong>
+                <p>{o.osStateName}</p>
+
+                <strong>Solicitante:</strong>
+                <p>{o.name}</p>
+
+                <strong>Data de abertura</strong>
+                <p>{o.rightInitDate}</p>
+
+                <strong>Descrição</strong>
+                <p>{o.osDescription}</p>
+
+                <strong>Local</strong>
+                <p>{o.localName}</p>
+
+                <strong>Equipamento(s)</strong>
+                <p>{o.equip}</p>
+
+                <strong>Data de encerramento</strong>
+                <p>{o.rightFinalDate}</p>
+              </li>
+            </Link>
+          ))}
+        </ul>
+      </Content>
+    </Container>
+  );
+};
+
+export default Locals;
---
+++ src/pages/Historico/styles.ts
@@ -0,0 +1,91 @@
+import styled, { keyframes } from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  align-items: stretch;
+
+  svg {
+    cursor: pointer;
+  }
+`;
+
+export const AsideContainer = styled.div`
+  h1 {
+    margin-bottom: 10px;
+  }
+`;
+
+export const Header = styled.div`
+  display: flex;
+  width: 100%;
+  height: 70px;
+  background: #1c1c1c;
+  align-items: center;
+  padding: 0 200px 0 200px;
+  justify-content: space-between;
+
+  h1 {
+    font-size: 15px;
+    color: #ff9000;
+  }
+`;
+
+export const ContentHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 10px 0 10px 0;
+
+  h1 {
+    font-size: 14px;
+    color: #fff;
+  }
+`;
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 200px;
+  align-self: center;
+  width: 70%;
+
+  a {
+    text-decoration: none;
+  }
+
+  ul {
+    width: 100%;
+    list-style: none;
+  }
+
+  li {
+    cursor: pointer;
+    width: 100%;
+    background: #4f4f4f;
+    padding: 20px;
+    border-radius: 10px;
+    margin-bottom: 20px;
+
+    strong {
+      color: #ff9000;
+    }
+
+    p {
+      padding: 5px 0 5px 0;
+      color: #fff;
+    }
+  }
+`;
+
+export const ButtonsContainer = styled.div`
+  display: flex;
+
+  svg {
+    margin-left: 20px;
+  }
+`;
+
+export const NameContainer = styled.div``;
